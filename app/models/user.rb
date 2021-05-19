860cb9dfc113b486e04018e677f0e24e4787379d
Ryan Erricson <ryanerricson@gmail.com>
2021-05-19 15:09:56 -0400

    Set up users model and sessions controller

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :email, presence: true, uniqueness: true
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+    validates
+end
