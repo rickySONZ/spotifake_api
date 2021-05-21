2dda4ed47fc6c045b7f0f3663251160cfecdd5bf
Ryan Erricson <ryanerricson@gmail.com>
2021-05-21 16:25:56 -0400

    Successfully fetching API for library on front end, changed routing structure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :liked_songs
+  
   resources :libraries
   resources :songs
-  resources :users, only: [:create, :index]
+  resources :users, only: [:create, :index] do
+    resources :libraries do
+      resources :liked_songs
+    end
+  end
   resources :sessions, only: [:create, :index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
