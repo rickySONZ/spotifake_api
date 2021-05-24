9f022398ef6ddaac92236d6f67446bbceacf7338
Ryan Erricson <ryanerricson@gmail.com>
2021-05-24 12:19:31 -0400

    Cleaned up library instatiation with find or create by method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
     def create 
         user = User.find_by_email(params[:email])
-        library = Library.find_by_user_id(user.id)
+        library = Library.find_or_create_by(user_id: user.id)
         if user && user.authenticate(params[:password]) && library
             session[:user_id] = user.id
             render json: {
@@ -11,15 +11,6 @@ class SessionsController < ApplicationController
                 email: user.email,
                 password: user.password,
                 library: library.id
-            }
-        elsif user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            library = Library.create(user_id: user.id)
-            render json: {
-                id: user.id,
-                status: 200,
-                email: user.email,
-                password: user.password
             }
         elsif user
             render json: {
