807adbd2d4117c38a10189d0be36a52fb6ad4590
Ryan Erricson <ryanerricson@gmail.com>
2021-06-30 16:41:04 -0400

    Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
-    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
     has_one :library
     has_many :liked_songs, through: :library
